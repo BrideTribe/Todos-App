7530c88a0d157303582a853e4b59b873a36f232d
BrideTribe <ritymontero@gmail.com>
2019-11-18 12:11:37 +0100

    fetch todos record from database

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,14 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Todos</title>
-</head>
-<body>
-    <h2>TODOS PAGE</h2>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Todos</title>
+    </head>
+    <body>
+        <h2>TODOS PAGE</h2>
 
-    {{ $todos }}
-</body>
+        <ul>
+            @foreach ($todos as $todo)
+                <li>
+                    {{ $todo->name }}
+                </li>
+            @endforeach
+        </ul>
+    </body>
 </html>
