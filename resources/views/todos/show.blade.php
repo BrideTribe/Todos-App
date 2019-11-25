e3af201da0c6c5f29fa050356a00a52b9b3bc9c5
BrideTribe <ritymontero@gmail.com>
2019-11-25 17:32:26 +0100

    widen the column size

--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -10,7 +10,7 @@
         {{ $todo -> name }}
     </h2>
     <div class="row justify-content-center">
-        <div class="col-md-6">
+        <div class="col-md-8">
             <div class="card card-default">
                 <div class="card card-header bg-secondary text-light">
                     <h3>Details</h3>
