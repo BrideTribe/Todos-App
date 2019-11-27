facf140e8bb67f35d5992b0929f34ae87ca9d60b
BrideTribe <ritymontero@gmail.com>
2019-11-27 10:45:40 +0100

    added edit function to update the todos

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -41,4 +41,29 @@ class TodosController extends Controller
 
         return redirect('/todos');
     }
+
+    public function edit($todoId)
+    {
+        return view('todos.edit')->with('todo', Todo::find($todoId));
+    }
+
+    public function update($todoId)
+    {
+        $this-> validate(request(), [
+            'name' => 'required|min:6|max:20',
+            'description' => 'required|max:150'
+        ]);
+
+        $data = request() ->all();
+
+        $todo = Todo::find($todoId);
+
+        $todo->name = $data['name'];
+        $todo->description = $data['description'];
+        
+        $todo->save(); 
+
+        return redirect('/todos');
+
+    }
 }
